f780fac0c9f560792cefca64b6e9b50866da735e
octaviogoncalvespt <octaviogoncalvespt@gmail.com>
2026-01-26 23:08:03 +0000

    Added Selects query with some data

---
+++ FinalProject02/Select.sql
@@ -0,0 +1,58 @@
+-- Simple Lists --
+
+
+-- All Tickets
+SELECT *
+FROM Tickets
+
+-- ALL users
+SELECT *
+FROM Users
+
+-- All Techs
+SELECT *
+FROM Techs
+
+-- Specific Selects
+
+-- Open Tickets
+SELECT *
+FROM Tickets
+WHERE EstadoAtualID = 1
+
+-- Closed Tickets
+SELECT *
+FROM Tickets
+WHERE EstadoAtualID = 5
+
+-- Tickets assigned to a specific Tech
+SELECT *
+FROM Tickets
+WHERE TechID = 2
+
+-- All Techs who worked on a specific Ticket
+SELECT TicketEstadoHistorico.TicketID, TicketEstadoHistorico.EstadoID, Techs.Nome
+FROM TicketEstadoHistorico
+    JOIN Techs ON TicketEstadoHistorico.TechID = Techs.ID
+WHERE TicketEstadoHistorico.TicketID = 4
+-- Use ID of ticket to check
+
+-- Users who have tickets and how many
+SELECT
+    Tickets.UserID,
+    Users.Nome AS Name,
+    COUNT(*) AS TicketCount
+FROM Tickets
+    JOIN Users ON Tickets.UserID = Users.ID
+GROUP BY Tickets.UserID, Users.Nome;
+
+-- Interventions for a specific Ticket
+SELECT *
+FROM TicketIntervencao
+WHERE TicketID = 7
+
+-- High Priority Open Tickets
+SELECT *
+FROM Tickets t
+WHERE t.EstadoAtualID BETWEEN 1 AND 3
+ORDER BY t.TicketPriorityID DESC;
